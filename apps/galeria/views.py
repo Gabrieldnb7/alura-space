e270e2912e3193b7979741e1b6562c5eca6ae4ce
Gabrieldnb7 <gabrieldnb7@gmail.com>
2023-08-30 18:53:29 -0300

    Ajustando Conflitos

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from galeria.models import Fotografia
+from apps.galeria.models import Fotografia
+from apps.galeria.forms import FotografiaForms
 from django.contrib import messages
 
 
@@ -29,3 +30,13 @@ def buscar(request):
 
 
     return render(request, 'galeria/buscar.html', {"cards": fotografias})
+
+def nova_imagem(request):
+    form = FotografiaForms
+    return render(request, 'galeria/nova_imagem.html', {"form": form})
+
+def editar_imagem(request):
+    pass
+
+def deletar_imagem(request):
+    pass
